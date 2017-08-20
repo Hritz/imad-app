ca0aca214367e0943a0bde37f3a194cffea7d8f6
Ritu Kashyap <ritukashyap212@gmail.com>
2017-08-20 15:34:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -93,9 +93,6 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/article-one', function (req, res) {
-  
-});
 
 app.get('/:articleName', function (req, res) {
     //articleName == article-one
@@ -104,9 +101,6 @@ app.get('/:articleName', function (req, res) {
   res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-three', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
 
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
