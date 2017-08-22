a8be9230ebad32a2606ab9c7493b1aca5b6bf622
Ritu Kashyap <ritukashyap212@gmail.com>
2017-08-22 16:23:27 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,4 +4,8 @@ console.log('Loaded!');
     var element = document.getElementById("main-text");
     element.innerHTML = "New Value";
     
-    
+    //move the image
+    var img = document.getElementById('madi');
+    img.onClick = function() {
+        img.style.marginLeft = '100px';
+    };
