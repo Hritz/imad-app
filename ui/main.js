dede8051fec0c6f58edeca8fecbdacc3b8f69dcc
Ritu Kashyap <ritukashyap212@gmail.com>
2017-08-23 00:31:09 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,59 @@
-console.log('Loaded!');
+//counter code
+var button = document.getElementById('counter');
+button.onclick = function() {
+    //create a request object
+    var request = new XMLHttpRequest();
+    //capture the response and store it in a variable
+        
+        if(request.readyState === XMLHttpRequest.DONE) {
+            //take somem action
+            if(request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+        // Not done yet - req phase
+        request.open('GET', 'http://ritukashyap212.imad.hasura-app.io/counter',true);
+        request.send(null);
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*console.log('Loaded!');
     
     //change the text of main-text div
     var element = document.getElementById("main-text");
@@ -16,4 +71,4 @@ console.log('Loaded!');
     img.onclick = function() {
         var interval = setInterval(moveRight, 10);
         
-    };
+    };*/
