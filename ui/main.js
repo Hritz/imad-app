3e193a36cedea8e8cfd8196cfe1dcf60b74c0396
Ritu Kashyap <ritukashyap212@gmail.com>
2017-08-27 01:06:34 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,28 +1,26 @@
-//counter code
-var button = document.getElementById('counter');
-button.onclick = function() {
-    
-    //create a request object
-    var request = new XMLHttpRequest();
-    
-    //capture the response and store it in a variable
-        request.onreadystatechange = function(){
-        if(request.readyState === XMLHttpRequest.DONE) {
+
+
+
+// submit names
+var nameInput = document.getElementById('name');
+var name  = nameInput.value;
+var submit = document.getElementById('submit_btn');
+    submit.onclick = function() {
+        
+        //make a request to the server annd send the names 
+        
+        //capture the list of names and render as a list
+        
+        var names = ['name1','name2','name3'];
+        var list = '';
+        for(var i=0; i<names.length; i++) {
             
-            //take somem action
-            if(request.status === 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-    };
-        // Not done yet - req phase
-        request.open('GET', 'http://ritukashyap212.imad.hasura-app.io/counter',true);
-        request.send(null);
+            list+ = '<li>'+ names[i] +'</li>' ;
+    }
+     var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+
 };
-
-
 
 
 
