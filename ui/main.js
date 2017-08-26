1eec70b61adf999e69a86ac902c0cbb1a8d329d9
Ritu Kashyap <ritukashyap212@gmail.com>
2017-08-27 01:10:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,4 +1,26 @@
-
+//counter code
+var button = document.getElementById('counter');
+button.onclick = function() {
+    
+    //create a request object
+    var request = new XMLHttpRequest();
+    
+    //capture the response and store it in a variable
+        request.onreadystatechange = function(){
+        if(request.readyState === XMLHttpRequest.DONE) {
+            
+            //take somem action
+            if(request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+    };
+        // Not done yet - req phase
+        request.open('GET', 'http://ritukashyap212.imad.hasura-app.io/counter',true);
+        request.send(null);
+};
 
 
 // submit names
