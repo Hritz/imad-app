f0152efdc67313625dc70565896b25bc0b71415b
Ritu Kashyap <ritukashyap212@gmail.com>
2017-09-01 18:57:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ app.get('/counter',function (req, res) {
 var names = [];
 app.get('/submit-name/:name', function (req, res) {
     //get the name from the request
-    var name = req.params.name;
+    var name = req.query.name;
     
     names.push(name);
     res.send(JSON.stringify(names));
