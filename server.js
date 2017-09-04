6a964f117590ab80f75631f27aa79c9426c978a5
Ritu Kashyap <ritukashyap212@gmail.com>
2017-09-05 01:47:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
 var express = require('express'); //commonly used to create servers
 var morgan = require('morgan'); //to help us see logs to our server 
 var path = require('path');
+var Pool = require('pg').Pool;
+
+var config = {
+            user: 'ritukashyap212',
+            database:'ritukashyap212',
+            host:'db.imad.hasura-app.io',
+            port:'5432',
+            password: process.env.DB_PASSWORD
+};
 
 var app = express();
 app.use(morgan('combined'));
@@ -92,6 +101,20 @@ function createTemplate(data){
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
+var pool = new Pool(config);
+app.get('/test-db', function(req, res){
+    
+    pool.query('SELECT * FROM test', function(err, result){
+        if(err){
+        res.status(500).send(err.toString());
+        } else{
+            res.send(JSON.stringify(result));
+        }
+    });
+});
+
+
 
 var counter = 0;
 app.get('/counter',function (req, res) {
